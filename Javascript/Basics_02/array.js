9127399d50ef845487ce0a101caa56758701710b
Sumit-sourabh45 <sumitsourabh9@gmail.com>
2025-12-29 03:25:17 +0530

    arrays and it's properties

--- Javascript/Basics_02/array.js
+++ Javascript/Basics_02/array.js
@@ -9,19 +9,20 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 // Array methods
 
-// myArr.push(6)
+myArr.push(45)
+console.log(myArr)
 // myArr.push(7)
 // myArr.pop()
 
-// myArr.unshift(9)
-// myArr.shift()
+myArr.unshift(9)    //adds element at the front
+myArr.shift()    //removes ele from front
 
-// console.log(myArr.includes(9));
+// console.log(myArr);
+// console.log(myArr.includes(9)); //checks whether the element exist in the array or Not?
 // console.log(myArr.indexOf(3));
 
 // const newArr = myArr.join()
 
-// console.log(myArr);
 // console.log( newArr);
 
 
@@ -32,9 +33,10 @@ console.log("A ", myArr);
 const myn1 = myArr.slice(1, 3)
 
 console.log(myn1);
+console.log("After slicing Array", myArr)
 console.log("B ", myArr);
 
 
 const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
 console.log(myn2);
+console.log("C -after splicing array becomes ", myArr);
