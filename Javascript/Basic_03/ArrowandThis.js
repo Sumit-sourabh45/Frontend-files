94c836c3b74cc5821c076880d2095f95146a0aa4
Sumit-sourabh45 <sumitsourabh9@gmail.com>
2025-12-30 23:51:44 +0530

    Arrow and This operator

---
+++ Javascript/Basic_03/ArrowandThis.js
@@ -0,0 +1,41 @@
+const user = {
+    username: "sumit",
+    price: 2100,
+    welcome: function(){
+        // console.log(`${this.username} welcome to website`)       //this refers to current context
+        // console.log(this)
+    }
+}
+user.welcome()
+user.username = "sam"
+user.welcome()
+
+// console.log(this)
+function chai(){                    //remeber, this cannot be used in function & arrow fn but it can be used in Objects
+    let username = "sumit"
+    console.log(this.username)
+}
+chai()
+
+const code = () =>{
+    let username = "sumit"
+    console.log(this);
+}
+code()
+
+const addNum = (num1, num2) => (num1 + num2)        //1st way
+
+
+// const addNum = (num1, num2) => {         //2nd way
+//     return num1 + num2
+// }
+console.log(addNum(4,5))
+
+const addOne = (num1, num2) => ({       //we need to wrap object in parenthesis () to execute if execute without return statement 
+    num1: 23,
+    num2: 45
+    
+})
+console.log(addOne(4,6))
+
+
---
+++ Javascript/Basic_03/function.js
@@ -0,0 +1,50 @@
+function heythere(){
+    console.log(23)
+}
+
+function addTwoNum(number1, number2){
+    // console.log(number1 + number2)
+
+    const result = number1 + number2
+    return result
+}
+const result = addTwoNum(5,6)
+// console.log(addTwoNum(3,4)) //this gives result but fn ain't returning any value so it's return undefined
+console.log(result)
+
+function loginUserMessage(username){
+    if(username === undefined){
+        console.log(`enter name pls`)
+        return
+    }
+
+    return `${username} just logged in`
+}
+console.log(loginUserMessage('sumit'))
+
+function addingItemsToCart(val1, ...num1){        //rest operator is used to add n no. of input to array
+    return num1
+}
+console.log(addingItemsToCart(100,400,24))
+
+//HANDLING OBJECT THROUGH FUNCTION
+const user = {
+    purchasaeItem: "Milk",
+    price: 200
+}
+function accessingUserObject(anyobject){
+    console.log(`the purchase item is: ${anyobject.purchasaeItem} and it's price is: ${anyobject.price}`);
+    
+}
+accessingUserObject(user)   //calling fn
+
+accessingUserObject({       //we can also pass object directly
+    purchasaeItem: "sumit",
+    price: 21
+})
+
+const array = [12, 34, 5]
+function returnarray(getarray){
+    return getarray
+}
+console.log(array)
