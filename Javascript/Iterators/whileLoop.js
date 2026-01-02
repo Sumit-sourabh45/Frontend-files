5708026b461d7dbe2e231213c3f33c27e13b9045
Sumit-sourabh45 <sumitsourabh9@gmail.com>
2026-01-02 21:35:13 +0530

    while loop

---
+++ Javascript/Iterators/whileLoop.js
@@ -0,0 +1,14 @@
+let myArray = ["tuntun", "bablu", "heera"]
+let arr = 0
+while (arr < myArray.length) {
+//    console.log(`valie of arr is ${myArray[arr]}`);
+    arr++
+}
+
+
+let value = 11
+do {
+    console.log(`value is ${value}`);
+    value++
+    
+} while (value <= 10);
