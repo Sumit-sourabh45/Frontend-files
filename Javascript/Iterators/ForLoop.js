0cb2941736ffead46fe6880b85208f1f6cc83f3c
Sumit-sourabh45 <sumitsourabh9@gmail.com>
2026-01-02 21:35:43 +0530

    for loop

---
+++ Javascript/Iterators/ForLoop.js
@@ -0,0 +1,28 @@
+// let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length);
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+// }
+
+//break and continue
+
+// for (let i = 1; i < 10; i++) {
+//     if (i == 5) {
+//         console.log("5 is found, STOP!");
+//         break
+        
+//     }
+//     console.log(`value of i is ${i}`);          //select and press shift+option+dwn arrow key to duplicate
+// }
+for (let i = 1; i < 10; i++) {
+    if (i == 5) {
+        console.log("5 is found, Let Go!");
+        continue
+        
+    }
+    console.log(`value of i is ${i}`);
+    
+    
+}
